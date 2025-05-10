31b97c17937370a7789653e27d7a56460bf93da1
neacsualexandra23 <neacsualexandra23@stud.ase.ro>
2025-05-10 10:08:49 +0300

    Laborator 6 - Hash table.c

--- Exercitiu14.c
+++ Exercitiu14.c
@@ -3,60 +3,49 @@
 #include<malloc.h>
 #include<stdio.h>
 #include<stdlib.h>
-
-struct Avion {
+/*
+struct Ferma {
 	char* denumire;
-	int nrLocuri;
-	int cod;
+	int nrAnimale;
+};
+struct nod {
+	struct Ferma info;
+	struct nod* next;
+	struct nod* prev;
 };
 
-void afisareAvion(struct Avion f) {
-	printf("denumirea este: %s \n", f.denumire);
-	printf("numarul locurilor este: %d \n", f.nrLocuri);
-	printf("codul este: %d \n", f.cod);
+struct listaDubla {
+	struct nod* first;
+	struct nod* last;
+	int nrNoduri;
+};
+
+struct Ferma citireOFermaDinFisier(FILE* fisier) {
+	struct Ferma f;
+	strtok
 }
 
-void eliberareMemorieAvion(struct Avion f) {
-	free(f.denumire);
-}
-void inserareOAvionVector(struct Avion f, struct Avion** vector, int index) {
-	(*vector)[index] = f;
-}
-
-struct Avion citesteOAvionDinFisier(FILE* fisier) {
-	struct Avion f;
-	char buffer[256];
-	fgets(buffer, 100, fisier);
-	char* aux;
-	aux = strtok(buffer, ",\n");
-	f.denumire = (char*)malloc(strlen(aux) + 1);
-	strcpy(f.denumire, aux);
-
-	aux = strtok(NULL, ",\n");
-	f.nrLocuri = atoi(aux);
-
-	aux = strtok(NULL, ",\n");
-	f.cod = atoi(aux);
-	return f;
-}
-void citesteToateAvioaneleDinFisierSiInserareInVector(char* numeFisier, struct Avion** vector, int dimensiune) {
-	FILE* fisier = fopen(numeFisier, "r");
-	(*vector) = (struct Ferma*)malloc(sizeof(struct Ferma*)*dimensiune);
-	int contor = 0;
-	while (contor < dimensiune && !feof(fisier)) {
-		struct Avion f = citesteOAvionDinFisier(fisier);
-		inserareOAvionVector(f, vector, contor);
-		contor++;
-	}
-	fclose(fisier);
-}
-void afisareVectorAvioane(struct Avion* vector, int dimensiune) {
-	for (int i = 0;i < dimensiune;i++)
-		afisareAvion(vector[i]);
+void inserareOListaDublaLaSfarsit(struct listaDubla* lista, struct Ferma f) {
+	struct nod* nou = (struct nod*)malloc(sizeof(struct nod));
+	nou->info = f;
+	nou->next = NULL;
+	nou->prev = lista->last;
+	if (lista->last)
+		lista->last->next = nou;
+	else
+		lista->first = nou;
+	lista->last = nou;
+	lista->nrNoduri++;
 }
 
-int main() {
-	struct Avion* vectorAvioane;
-	citesteToateAvioaneleDinFisierSiInserareInVector("Avioane.txt", &vectorAvioane, 5);
-	afisareVectorAvioane(vectorAvioane, 5);
+void citireToateFermeleDinFisier
+
+void afisareListaDulblaDeLaInceput(struct listaDubla lista) {
+	struct nod* p= lista.last
+		while (p) {
+			sfisareFerme(p->info)
+				p-p>next
+		}
 }
+
+*/
